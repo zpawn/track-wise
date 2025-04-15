66b2db0d6c49970bd6a49b573463801d02054af5
ilia makarov <ilia.makarov@me.com>
2025-04-15 21:45:23 +0300

    configured for vercel deployment

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 5001
 
-CMD ["npm", "start"]
+CMD ["npm", "start:dev"]
--- api/app.js
+++ api/app.js
@@ -22,17 +22,10 @@ fastify.get('/', async (req, res) => {
   return res.status(200).type("text/html").send("<h1>Hello, Fastify!</h1>");
 })
 
-const start = async () => {
-  try {
-    const port = process.env.PORT || 5001;
-    const host = process.env.HOST || "0.0.0.0";
-
-    await fastify.listen({ port, host });
-    fastify.log.info(`Server is running on post ${fastify.server.address().port}`);
-  } catch (error) {
-    fastify.log.error(error);
-    process.exit(1);
-  }
+async function handler(req, reply) {
+  await fastify.ready()
+  fastify.server.emit('request', req, reply)
 };
 
-start();
+module.exports = handler;
+module.exports.fastify = fastify;
