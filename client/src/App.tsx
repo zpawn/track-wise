dd0d002a795d7292ed31d7b945f268df0a31c30b
ilia makarov <ilia.makarov@me.com>
2025-04-19 19:52:15 +0300

    Configured shadcn/ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { LoginForm } from "@/components/login-form";
+
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold underline">Hello React, Fastify & Genezio!</h1>
+      <LoginForm/>
     </div>
   )
 };
